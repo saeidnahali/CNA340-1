3fd4789132726588cbdbbdbfe22ae05706ac967f
saeidnahali <63702251+saeidnahali@users.noreply.github.com>
2020-06-01 21:48:10 -0700

    I add and remove code
    I add 1,2,3 line and I created new code at  line 6 and I add print line at line 16

--- Pull_Request/debug.py
+++ Pull_Request/debug.py
@@ -1,11 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
 def lone_sum(a, b, c):
-    if a >= b:
+    if not (not (a == b) or not (a == c) or not (b == c)):
+        return 0
+    elif a >= b:
         return c
     elif a == c:
         return b
     elif b == c:
         return a
-    elif a == b and a == c and b == c:
-        return 0
     else:
         return a+b+c
+print(lone_sum(a, b, c))
